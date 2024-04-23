c54a8ed0a46422edd9d9e5d4b541cb6b838c8078
Gaurav Meena <gauravmeena0708@gmail.com>
2024-04-24 01:36:03 +0530

    test

--- tests/test_module1.py
+++ tests/test_module1.py
@@ -6,7 +6,7 @@ import unittest
 class TestSimple(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual((Number(5) + Number(6)).value, 11)
+        self.assertEqual((5) + (6)), 11)
 
 
 if __name__ == '__main__':
